00932090a2290e41f51bb1d5d8da36a571bb3fda
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-09-15 12:49:38 -0700

    create draft

--- main.js
+++ main.js
@@ -8,12 +8,62 @@
     b. Bob, 23, 24
     c. Nick, 40, 35
     d. Alex, 44, 45
-2. Suppose that, students have chance to get different degrees of diploma, like A, B, C, D, E and those degrees are relevant
-    to the following passing limits 91, 81, 71, 61, 51. According to that, create two arrays for passing limits and for degrees
+2. Suppose that, students have chance to get different degrees of diploma, like A, B, C, D, E and those degrees are relevant to the following passing limits 91, 81, 71, 61, 51. According to that, create two arrays for passing limits and for degrees
 3. Create function which will calculate total score (score1 + score2) for each student.
-4. Create function and use in it for loops, if else statements and whatever you need, in order to figure out, which student 
-    has passed an exam and what kind of degree he has got.
+4. Create function and use in it for loops, if else statements and whatever you need, in order to figure out, which student has passed an exam and what kind of degree he has got.
 5. Display the final result in console.
 */
 
 // SOLUTION:
+// 1.
+const students = [
+  {
+    name: 'John',
+    score1: 47,
+    score2: 46,
+  },
+  {
+    name: 'Bob',
+    score1: 23,
+    score2: 24,
+  },
+  {
+    name: 'Nick',
+    score1: 40,
+    score2: 35,
+  },
+  {
+    name: 'Alex',
+    score1: 44,
+    score2: 45,
+  },
+];
+console.log(students);
+
+// 2.
+const degrees = ['A', 'B', 'C', 'D', 'E'];
+const passingLimit = [91, 81, 71, 61, 51];
+
+// 3. 4.
+const calcTotalScore = function () {
+  for (let i = 0; i < students.length; i++) {
+    const totalScore = students[i].score1 + students[i].score2;
+    const studentsName = students[i].name;
+    // console.log(studentsName, totalScore);
+
+    let studentsDegree = '';
+
+    for (let i = 0; i < students.length; i++) {
+      if (totalScore > passingLimit[i]) {
+        studentsDegree = degrees[i];
+        // console.log(studentsDegree);
+        break;
+      } else {
+          studentsDegree = 'no degree'
+      }
+    }
+    
+    console.log(`${students[i].name} => total score: ${totalScore}, degree: ${studentsDegree}`)
+  }
+};
+calcTotalScore();
